a2168c18a0badb66bfdbe882a98d158504e4d75b
mmunozrojas <mantoniomunozrojas@gmail.com>
2023-06-24 02:20:48 -0400

    Se agrega efecto hover a las card de CursosComp.vue

--- src/components/CursosComp.vue
+++ src/components/CursosComp.vue
@@ -1,39 +1,49 @@
 <template>
   <v-container class="container" fluid>
-    <v-card class="course-card ma-1 hover elevation-2">
-      <div class="image-container">
-        <v-img :src="curso.urlImagen" height="200px" contain></v-img>
-      </div>
-      <v-card-title>
-        <v-spacer />
-        <div class="text-center">
-          <h4 class="headline title">{{ curso.nombre }}</h4>
+    <v-hover v-slot="{ hover }">
+      <v-card class="course-card ma-1 hover elevation-2">
+        <div class="image-container">
+          <v-img :src="curso.urlImagen" height="200px" contain>
+            <v-expand-transition>
+              <div v-if="hover" class="d-flex transition-fast-in-fast-out v-card--reveal align-center justify-center"
+                style="background-color: rgba(255, 87, 34, 0.8); height: 100%;">
+                <v-btn color="deep-purple" dark>
+                  Inscribirme ahora
+                </v-btn>
+              </div>
+            </v-expand-transition>
+          </v-img>
         </div>
-        <v-spacer />
-      </v-card-title>
-      <v-card-title class="headline text-center"></v-card-title>
-      <v-card-subtitle class="text-center"></v-card-subtitle>
-      <v-card-text class="text-center">
-        <div class="subtitle-2 my-1 course-data text-center">
-          <v-icon class="icon-cost" color="blue">mdi-currency-usd</v-icon>
-          <strong class="highlight-text">Costo:</strong> {{ formattedCosto }}
-        </div>
-        <div class="subtitle-2 my-1 course-data text-center">
-          <v-icon class="icon-duration" color="green">mdi-clock-outline</v-icon>
-          <strong class="highlight-text">Duración:</strong> {{ curso.duracion }}
-        </div>
-        <div class="subtitle-2 my-1 course-data text-center">
-          <v-icon class="icon-capacity" color="orange">mdi-account-group-outline</v-icon>
-          <strong class="highlight-text">Cupos:</strong> {{ curso.cupos }}
-        </div>
-        <div class="subtitle-2 my-1 course-data text-center">
-          <v-icon class="icon-enrollment" color="purple">mdi-account-check-outline</v-icon>
-          <strong class="highlight-text">Inscritos:</strong> {{ curso.inscritos }}
-        </div>
-      </v-card-text>
-    </v-card>
+        <v-card-title>
+          <v-spacer />
+          <div class="text-center">
+            <h4 class="headline title">{{ curso.nombre }}</h4>
+          </div>
+          <v-spacer />
+        </v-card-title>
+        <v-card-text class="text-center">
+          <div class="subtitle-2 my-1 course-data text-center">
+            <v-icon class="icon-cost" color="blue">mdi-currency-usd</v-icon>
+            <strong class="highlight-text">Costo:</strong> {{ formattedCosto }}
+          </div>
+          <div class="subtitle-2 my-1 course-data text-center">
+            <v-icon class="icon-duration" color="green">mdi-clock-outline</v-icon>
+            <strong class="highlight-text">Duración:</strong> {{ curso.duracion }}
+          </div>
+          <div class="subtitle-2 my-1 course-data text-center">
+            <v-icon class="icon-capacity" color="orange">mdi-account-group-outline</v-icon>
+            <strong class="highlight-text">Cupos:</strong> {{ curso.cupos }}
+          </div>
+          <div class="subtitle-2 my-1 course-data text-center">
+            <v-icon class="icon-enrollment" color="purple">mdi-account-check-outline</v-icon>
+            <strong class="highlight-text">Inscritos:</strong> {{ curso.inscritos }}
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-hover>
   </v-container>
 </template>
+
 
 <script>
 export default {
@@ -79,12 +89,6 @@ export default {
 
 .image-container {
   height: 200px;
-}
-
-.image-container img {
-  height: 90%;
-  width: 100%;
-  object-fit: contain;
 }
 
 .course-data {
